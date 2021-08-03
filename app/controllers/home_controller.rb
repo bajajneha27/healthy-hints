33b2dcf5cbead3d49744685223346c723e838205
Neha Bajaj <bajajnehaa@google.com>
2021-08-03 19:09:08 +0530

    Adding firestore gem.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,5 @@
 require "google/cloud/vision"
+require "google/cloud/firestore"
 
 class HomeController < ApplicationController
 
@@ -8,11 +9,25 @@ class HomeController < ApplicationController
 
 	def analyse
 		data_uri = params[:data_uri] || ''
+
+		project_id = 'healthy-hints-321005'
 		ENV['GOOGLE_APPLICATION_CREDENTIALS'] = "/Users/bajajnehaa/Downloads/healthy-hints-321005-ee275d5a467c.json"
-		ENV['GOOGLE_CLOUD_PROJECT'] = "healthy-hints-321005"
+		ENV['GOOGLE_CLOUD_PROJECT'] = project_id
+
 		image_annotator = Google::Cloud::Vision.image_annotator
 		file_name = "/Users/bajajnehaa/gcp/ruby-docs-samples/vision/images/serum.png"
 		res = image_annotator.text_detection image: file_name
+
+		firestore = Google::Cloud::Firestore.new project_id: project_id
+		col = firestore.collections.first
+		query = col.where 'name', 'IN', ['glycerin', 'linalool']
+
+		query.get do |res|
+			res.fields
+			puts res.fields
+		end
+
+
 		render :json => res.responses.first.text_annotations.map(&:description)
 	end
 end
