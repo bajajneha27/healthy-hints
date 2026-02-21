bbd836cfdcd2808b7959e1f7b7ae389cef127bc6
bajajnehaa <bajajnehaa@google.com>
2026-02-21 09:25:02 +0000

    feat: integrate with google-genai LLM

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
+from dotenv import load_dotenv
+from google import genai
+import os
+import json
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
+
+# Load environment variables
+load_dotenv()
 
 app = FastAPI()
+
+# Get API key from .env
+api_key = os.getenv("GOOGLE_API_KEY")
+
+# Initialize Gemini client
+client = genai.Client(api_key=api_key)
 
 # Define request body schema
 class IngredientRequest(BaseModel):
@@ -13,15 +30,40 @@ def read_root():
 
 @app.post("/analyse")
 def analyse(data: IngredientRequest):
-  ingredients_list = [i.strip() for i in data.ingredients_text.split(",")]
+  prompt = """
+  You are a food ingredient health expert.
+  Analyze the following ingredient list:
 
-  analyzed = [
+  INGREDIENTS_PLACEHOLDER
+  Return ONLY valid JSON in this format:
+  {
+    "ingredients": [
     {
-      "name": ingredient,
-      "health_rating": "Moderate",
-      "short_explanation": "Example explanation."
-    }
-    for ingredient in ingredients_list
-  ]
+      "name": "",
+      "category": "",
+      "health_rating": "",
+      "short_explanation": ""
+    }],
+    "overall_score": 1-10,
+    "summary": ""
+  }
+  """
 
-  return analyzed
+  logger.debug(f"Received input: {data.ingredients_text}")
+  prompt = prompt.replace("INGREDIENTS_PLACEHOLDER", data.ingredients_text)
+
+  try:
+    response = client.models.generate_content(
+      model="gemini-flash-latest",
+      contents=prompt,
+      config={
+        "temperature": 0.2,
+        "response_mime_type": "application/json"
+      }
+    )
+    logger.debug(f"Gemini response: {response}")
+    parsed = json.loads(response.text)
+  except Exception as e:
+    logger.exception("Error occurred in /analyse")
+    raise e
+  return parsed
